a10920c6bf9fa11f062ad27d6a4ff9e81d518cb9
Özlem Muslu <oezlem.muslu@tron-mainz.de>
2025-05-05 10:30:00 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,66 @@
 #!/usr/bin/env bash
 
 set -e
+set -o pipefail  # Catch failures in piped commands
 
 . $1
 
 mkdir -p ${ENV_FOLDER}
+
+
+### download models
+cd ${CODE_FOLDER}/models
+
+URL="https://huggingface.co/tron-mainz/3ddensenet_snv/resolve/main/3ddensenet_snv.pt"
+FILENAME="3ddensenet_snv.pt"
+
+echo "Downloading $FILENAME from $URL..."
+
+if ! curl -L --retry 5 --retry-delay 2 --retry-max-time 60 --fail -o "$FILENAME" "$URL"; then
+  echo "Error: Failed to download $FILENAME from $URL" >&2
+  exit 1
+fi
+
+echo "Download successful: $FILENAME"
+
+URL="https://huggingface.co/tron-mainz/3ddensenet_indel/blob/main/3ddensenet_indel.pt"
+FILENAME="3ddensenet_indel.pt"
+
+echo "Downloading $FILENAME from $URL..."
+
+if ! curl -L --retry 5 --retry-delay 2 --retry-max-time 60 --fail -o "$FILENAME" "$URL"; then
+  echo "Error: Failed to download $FILENAME from $URL" >&2
+  exit 1
+fi
+
+echo "Download successful: $FILENAME"
+
+URL="https://huggingface.co/tron-mainz/extra_trees.indel/blob/main/extra_trees.indel.joblib"
+FILENAME="extra_trees.indel.joblib"
+
+echo "Downloading $FILENAME from $URL..."
+
+if ! curl -L --retry 5 --retry-delay 2 --retry-max-time 60 --fail -o "$FILENAME" "$URL"; then
+  echo "Error: Failed to download $FILENAME from $URL" >&2
+  exit 1
+fi
+
+echo "Download successful: $FILENAME"
+
+URL="https://huggingface.co/tron-mainz/extra_trees.snv/blob/main/extra_trees.snv.joblib"
+FILENAME="extra_trees.snv.joblib"
+
+echo "Downloading $FILENAME from $URL..."
+
+if ! curl -L --retry 5 --retry-delay 2 --retry-max-time 60 --fail -o "$FILENAME" "$URL"; then
+  echo "Error: Failed to download $FILENAME from $URL" >&2
+  exit 1
+fi
+
+echo "Download successful: $FILENAME"
+
+cd ${CODE_FOLDER}
+
 
 ### create conda envs
 echo "Creating variantmedium environment"
